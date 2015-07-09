8353c4935a56d3c797daf829ea8456a9cf114261
alessandrotraversi <alessandro.traversi@hotmail.it>
2015-07-08 22:33:01 -0300

    metodo fillable - impostare il controller per ricevere dati - esibirenelle view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+Use App\Post;
 use App\Http\Controllers\Controller;
 
 class PostsController extends Controller
 {
     public function index(){
-        return view('posts.index');
+        $post = \App\Post::all();
+        return view('posts.index', compact('posts'));
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,8 +3,12 @@
 
 
 @section('content')
-<h1>Post</h1><br />
+<h1>Post</h1><br /><br />
 
-
+@foreach($posts as $post)
+<h2>{{ $post->title }}</h2><br />
+<p>{{ $post->content }}</p><br /><br />
+<hr />
+@endforeach
 
 @endsection
