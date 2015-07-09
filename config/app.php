eb3791e71f6d1a46318426ba1620a5b0f5a3cbe2
alessandrotraversi <alessandro.traversi@hotmail.it>
2015-07-09 17:44:10 -0300

    impostando form - rinominando e raggruppando ROUTE

--- config/app.php
+++ config/app.php
@@ -143,7 +143,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \Illuminate\Html\htmlServiceProvider::class
+        Illuminate\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -191,8 +191,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form'      => \Illuminate\Html\FormFacades::class,
-        'Html'      => \Illuminate\Html\HtmlFacades::class
+        'Form'      => Illuminate\Html\FormFacade::class,
+        'Html'      => Illuminate\Html\HtmlFacade::class,
 
     ],
 
