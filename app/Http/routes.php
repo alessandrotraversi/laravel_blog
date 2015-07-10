61e46867bee38f3600519d483ef66f059f5a078e
alessandrotraversi <alessandro.traversi@hotmail.it>
2015-07-09 22:51:19 -0300

    ereditando le views e le ROUTE di Laravel 5.0

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,6 +27,9 @@ Route::group(['prefix'=>'admin', 'middleware'=>'auth'], function(){
 });
 
 
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::controllers([
+
+    'auth'=>'Auth\AuthController',
+    'password'=>'Auth\PasswordController',
+
+]);
