133c4a5d25eb1d0c676f0364f22458efcc1e2af7
alessandrotraversi <alessandro.traversi@hotmail.it>
2015-07-09 21:39:18 -0300

    configurando il metodo edit e update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,8 +19,8 @@ Route::group(['prefix'=>'admin'], function(){
         Route::get('',['as'=>'admin.posts.index', 'uses'=>'PostsAdminController@index']);
         Route::get('create',['as'=>'admin.posts.create', 'uses'=>'PostsAdminController@create']);
         Route::post('store',['as'=>'admin.posts.store', 'uses'=>'PostsAdminController@store']);
-        //Route::get('edit/{id}',['as'=>'admin.posts.edit', 'uses'=>'PostsAdminController@edit']);
-        //Route::put('update/{id}',['as'=>'admin.posts.update', 'uses'=>'PostsAdminController@update']);
+        Route::get('edit/{id}',['as'=>'admin.posts.edit', 'uses'=>'PostsAdminController@edit']);
+        Route::put('update/{id}',['as'=>'admin.posts.update', 'uses'=>'PostsAdminController@update']);
         //Route::get('destroy/{id}',['as'=>'admin.posts.destroy', 'uses'=>'PostsAdminController@destroy']);
         
     });
