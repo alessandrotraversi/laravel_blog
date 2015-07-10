462d7f45e9716b5b91b78ebac942e7fd90af2014
alessandrotraversi <alessandro.traversi@hotmail.it>
2015-07-09 21:46:06 -0300

    concludere la paginazione

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,20 +5,25 @@
 @section('content')
 <h1>Sezione amministrativa</h1>
 
+<a href="{{route('admin.posts.create')}}" class="btn btn-success">Create new post</a><br /><br />
+ 
 <table class="table">
-    <tr>
+     <tr>
         <td>ID</td>
         <td>Title</td>
         <td>Action</td>
-    </tr>
-    @foreach($posts as $post)
-    <tr>
-        <td>{{ $post->id }}</td>
-        <td>{{ $post->title }}</td>
-        <td></td>
-    </tr>
-    @endforeach
-</table>
+     </tr>
+     @foreach($posts as $post)
+     <tr>
+         <td>{{$post->id}}</td>
+         <td>{{$post->title}}</td>
+         <td>
+             <a href="{{route('admin.posts.edit',['id'=>$post->id])}}" class="btn btn-default">Edit</a>
+             <a href="{{route('admin.posts.destroy',['id'=>$post->id])}}" class="btn btn-danger">Delete</a>
+         </td>
+     </tr>
+     @endforeach
+ </table>
 
 {!! $posts->render() !!}
 
